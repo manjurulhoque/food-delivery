3d31d4c07362f8e2b3f025a85a79ad5a12e476af
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-09-25 00:20:04 +0600

    Added django to order service and created order and order item models

---
+++ order-service/core/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class OrderStatus(models.TextChoices):
+    PENDING = 'PENDING', _('Pending')
+    CONFIRMED = 'CONFIRMED', _('Confirmed')
+    CANCELED = 'CANCELED', _('Canceled')
+    DELIVERED = 'DELIVERED', _('Delivered')
+
+
+class Order(models.Model):
+    customer_id = models.IntegerField()
+    restaurant_id = models.IntegerField()
+    total_price = models.FloatField()
+    status = models.CharField(max_length=100, default=OrderStatus.PENDING, choices=OrderStatus.choices)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Order {self.id}'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    item_name = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f'OrderItem {self.id}'
