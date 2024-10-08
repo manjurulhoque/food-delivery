e13c28f8c9a1ee287d87615ecfd429eec80ceb18
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-10-08 22:04:36 +0600

    Updated root URL of services

--- auth-service/auth/urls.py
+++ auth-service/auth/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/auth/', include([
+    path('', include([
         path('', include('core.urls')),
         path('admin/', admin.site.urls),
     ])),
