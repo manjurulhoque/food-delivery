a7aa7856cb9ccae412ad6001ac7e21f4ede96f80
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-11-22 16:52:32 +0600

    Added validation before registration

--- auth-service/core/serializers.py
+++ auth-service/core/serializers.py
@@ -18,16 +18,24 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+
     class Meta:
         model = User
         fields = ("email", "password", "is_customer", "is_restaurant", "is_driver")
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("This email is already in use.")
+        return value
 
     def create(self, validated_data):
         user = User.objects.create_user(
             email=validated_data["email"],
             password=validated_data["password"],
-            is_customer=validated_data["is_customer"],
-            is_restaurant=validated_data["is_restaurant"],
-            is_driver=validated_data["is_driver"],
+            is_customer=validated_data.get("is_customer", False),
+            is_restaurant=validated_data.get("is_restaurant", False),
+            is_driver=validated_data.get("is_driver", False),
         )
         return user
